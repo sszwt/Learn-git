eb4eae2154a6f2268a256efcfc19ca0031ca78be
Krunal Balas <krunal.balas@zealousweb.com>
2025-08-05 17:00:31 +0530

    dg

--- test2.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'Hello, this is test2.php!';
-echo 'Hello, this is test2.php!';
-echo 'Hello, this is test2.php!';
-echo 'Hello, this is test2.php!';
-echo 'Hello, this is test2.php!';
-?>
